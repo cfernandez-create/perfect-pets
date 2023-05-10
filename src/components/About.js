38d308ac1366a8de6a793b6ce4cdd253c2e9ea5d
cfernandez-create <131753147+cfernandez-create@users.noreply.github.com>
2023-05-09 20:11:35 -0700

    testing image path

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function About(){
     return(
         <div className="main--heading">
-            <img src="/public/img/everydaylife.png" className="title--image" alt="title-image"/>
+            <img src="public/img/everydaylife.png" className="title--image" alt="title-image"/>
             <h3 className="tagline">"Changing the world, one wagging tail at a time. Adopt from our rescue."</h3>
         
 <h1><strong>Available Dogs</strong></h1>
